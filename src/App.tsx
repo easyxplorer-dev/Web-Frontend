ae980ca8700e71eb65fa5296a20222b8f7c3415b
Junior Ruiz <junior31064049@gmail.com>
2024-09-07 11:52:21 -0500

    Remove Material UI library and added daysi UI instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-    <Navbar />
-    </>
-  )
+    <main className="bg-gray-100 min-h-screen">
+      <Navbar />
+    </main>
+  );
 }
 
-export default App
+export default App;
